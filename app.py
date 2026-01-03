b702ac7a5985fd924fb8869c78d076bcaecfd262
masterpro-com <toshpolatovm71@gmail.com>
2026-01-03 08:52:34 +0500

    bot.py

--- app.py
+++ app.py
@@ -1,21 +1,61 @@
 import telebot
+import random
+import os
 
-TOKEN = "8528236172:AAFQ3st4r32JgtRf8ZhQGDZe69aAcwngruw"
+TOKEN = os.getenv("8528236172:AAFQ3st4r32JgtRf8ZhQGDZe69aAcwngruw")
 
 bot = telebot.TeleBot(TOKEN)
+games = {}  # user_id: number
 
-# /start buyrug‘i
 @bot.message_handler(commands=['start'])
-def start_message(message):
+def start(message):
     bot.send_message(
         message.chat.id,
-        "Salom 👋\nBu mening birinchi Telegram botim 🤖"
+        "🎮 Son topish o‘yiniga xush kelibsiz!\n\n"
+        "1 dan 100 gacha son o‘ylayman.\n"
+        "Topishga harakat qiling!\n\n"
+        "Boshlash uchun /game ni bosing."
     )
 
-# Oddiy xabarlarni qaytarish (echo)
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.send_message(message.chat.id, f"Siz yozdingiz: {message.text}")
+@bot.message_handler(commands=['game'])
+def new_game(message):
+    number = random.randint(1, 100)
+    games[message.from_user.id] = number
+    bot.send_message(
+        message.chat.id,
+        "🤫 Men son o‘yladim!\n"
+        "Endi 1–100 oralig‘ida taxmin qiling."
+    )
 
-print("Bot ishga tushdi...")
+@bot.message_handler(func=lambda m: m.text.isdigit())
+def guess(message):
+    user_id = message.from_user.id
+
+    if user_id not in games:
+        bot.send_message(message.chat.id, "❗ Avval /game ni boshlang.")
+        return
+
+    g = int(message.text)
+    number = games[user_id]
+
+    if g < number:
+        bot.send_message(message.chat.id, "⬆️ Kattaroq son")
+    elif g > number:
+        bot.send_message(message.chat.id, "⬇️ Kichikroq son")
+    else:
+        bot.send_message(
+            message.chat.id,
+            f"🎉 TOPDINGIZ!\nSon: {number}\n\n"
+            "Yana o‘ynash uchun /game"
+        )
+        del games[user_id]
+
+@bot.message_handler(func=lambda m: True)
+def other(message):
+    bot.send_message(
+        message.chat.id,
+        "🎯 Raqam yozing yoki /game buyrug‘ini bosing."
+    )
+
+print("🎮 Bot ishga tushdi...")
 bot.polling()
